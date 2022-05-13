3e4d3722bea0e1043841c4a20f812dc3db0449c1
Shadow <ekostukevich@gmail.com>
2022-05-13 19:46:51 +0400

    test with properties

--- dictionary/src/main/kotlin/shadow/system/dictionary/Application.kt
+++ dictionary/src/main/kotlin/shadow/system/dictionary/Application.kt
@@ -2,8 +2,11 @@ package shadow.system.dictionary
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import shadow.system.dictionary.config.PostgresConfig
 
 @SpringBootApplication(scanBasePackages = ["shadow.system"])
+@EnableConfigurationProperties(value = [ PostgresConfig::class ])
 open class Application
 
 fun main(args: Array<String>) {
